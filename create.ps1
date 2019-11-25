08e46ce09bf65ecc338cf08f0c4c5cc9b430d148
Michael Chew <michael.chew@actifio.com>
2019-11-25 14:11:20 +1100

    Update create.ps1

--- create.ps1
+++ create.ps1
@@ -1,1 +1,11 @@
+##
 write-output "Hello world"
+
+if (! $env:ACTSESSIONID ){
+  Connect-Act -acthost $Env:ActifioIP -actuser $Env:ActUser -password $Env:Password -ignorecerts -quiet
+}
+
+udsinfo lsversion 
+
+Disconnect-Act | Out-Null
+ 
