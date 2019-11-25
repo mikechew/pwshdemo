7af18caf98052a371dce349f585b08334bff7259
Michael Chew <michael.chew@actifio.com>
2019-11-25 14:26:49 +1100

    Update create.ps1

--- create.ps1
+++ create.ps1
@@ -1,6 +1,10 @@
 ##
 write-output "Hello world"
 
+.\"$Env:WORKSPACE\actfunc".ps1
+
+ActPC
+
 if (! $env:ACTSESSIONID ){
   Connect-Act -acthost $Env:ActifioIP -actuser $Env:ActUser -password $Env:ActPassword -ignorecerts -quiet
 }
